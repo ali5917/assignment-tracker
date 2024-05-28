7556625cab69fe1f4bf4268edcd8a51d222509bb
Ali Kashif <alikashif5917@gmail.com>
2024-05-28 21:56:00 +0500

    App setup and Added Title and favicon to index.html

---
+++ app.py
@@ -0,0 +1,29 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "This is about page"
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
